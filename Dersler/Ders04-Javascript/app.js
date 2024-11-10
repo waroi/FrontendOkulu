756bf3762bb22775ab9bc5b1043f4f8b3887030b
waroi <varolmaksutoglu@yandex.com>
2024-11-10 11:06:36 +0300

    js giriş

---
+++ Dersler/Ders04-Javascript/app.js
@@ -0,0 +1,108 @@
+// JS Giriş
+
+// console.log("Merhaba Javascript");
+
+// Değişkenler
+// Number
+// var a = 10; // Number
+// var b = 3.14; // Number
+
+// String
+// var c = "Javascript"; // String
+
+// console.log(a);
+// console.log(typeof a);
+
+// console.log(a, b, c);
+
+// otomatik tip dönüşümleri
+// var firstNumber = 10;
+// var secondNumber = "20";
+// var result = firstNumber - secondNumber;
+// var result = firstNumber + secondNumber;
+// console.log(result);
+// console.log(typeof result);
+
+// hoisting!
+// var firstNumber;
+// console.log(firstNumber);
+// firstNumber = 20;
+// deneme();
+// function deneme() {
+//   console.log("a fonksiyonu");
+// }
+
+// Boolean (true/false)
+
+// var isActive = true;
+
+// if (age) {
+//   age >= 18 ? console.log("Ehliyet alabilir") : console.log("Ehliyet alamaz");
+// }
+
+// Undefined - Tanımsız
+// var userName;
+// console.log(userName);
+// console.log(typeof userName);
+
+// Null - Değer yok
+// var isNull = null;
+// console.log(isNull);
+// console.log(typeof isNull);
+
+// Object - Nesne!
+// var user = {
+//   name: "Hakan",
+//   age: 30,
+//   isActive: true,
+//   notlar: {
+//     mat: 90,
+//     fizik: 80,
+//   },
+//   favariteFruits: ["elma", "armut"],
+// };
+
+// console.log(user);
+// console.log(typeof user);
+// console.log(user.name);
+// console.log(user.notlar.mat);
+
+// Array - Dizi !
+// var fruits = ["elma", "armut", "muz"];
+// console.log(fruits);
+// console.log(typeof fruits);
+// console.log(fruits[0]);
+// console.log(fruits[fruits.length - 1]);
+
+// function - Fonksiyonlar
+// function hello() {
+//   console.log("Merhaba Javascript");
+// }
+// hello();
+// console.log(hello);
+// console.log(typeof hello);
+
+// Date - Tarih
+// var date = new Date();
+// console.log(date);
+// console.log(typeof date);
+
+// var a = 10;
+// var b = a;
+// console.log("1- ", a, b);
+// a = 20;
+// console.log("2- ", a, b);
+
+// var a = [10];
+// var b = a;
+// console.log("1- ", a, b);
+// a = [20];
+// console.log("2- ", a, b);
+// console.log("3- ", a * b);
+
+// var a = [10, 20, 30, 40, 50];
+// var b = a;
+// console.log("1- ", a, b);
+//  a[0] = 60;
+//  a.push(60);
+// console.log("2- ", a, b);
