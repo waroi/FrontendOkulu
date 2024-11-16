ceacffa90735704f5f30b7a31982f9ad495c4626
waroi <varolmaksutoglu@yandex.com>
2024-11-16 11:06:45 +0300

    oop

---
+++ Dersler/Ders05-Javascript-OOP/01-prototype.js
@@ -0,0 +1,53 @@
+// Object Literal
+// const person1 = {
+//   name: "Varol",
+//   age: 30,
+//   langs: ["Java", "Javascript", "Python"],
+//   address: {
+//     city: "Istanbul",
+//     street: "Kadikoy",
+//   },
+//   showInfos: function () {
+//     console.log(this.name, this.age, this.langs, this.address);
+//   },
+// };
+
+// const person2 = {
+//   name: "Mehmet",
+//   age: 31,
+//   langs: ["Go", "Javascript", "Python"],
+//   address: {
+//     city: "Ankara",
+//     street: "Cankaya",
+//   },
+//   showInfos: function () {
+//     console.log(this.name, this.age, this.langs, this.address);
+//   },
+// };
+
+// console.log(person1);
+// console.log(person2);
+
+// Yapıcı Fonksiyon (Constructor)
+function Person(name, age, langs) {
+  this.name = name;
+  this.age = age;
+  this.langs = langs;
+
+  // this.showInfos = function () {
+  //   console.log(this.name, this.age, this.langs);
+  // };
+}
+
+Person.prototype.showInfos = function () {
+  console.log(this.name, this.age, this.langs);
+};
+
+const person1 = new Person("Varol", 30, ["Java", "Javascript", "Python"]);
+const person2 = new Person("Mehmet", 31, ["Go", "Javascript", "Python"]);
+
+console.log(person1);
+console.log(person2);
+person1.showInfos();
+person2.showInfos();
+console.log(person1.langs[0]);
